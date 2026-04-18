c5e036041ce5c334a6de338b588cbb25449231ee
Samuel Åkesson <samak519@student.liu.se>
2026-04-18 17:03:09 +0200

    move gamepad canvas painting into method

--- src/app.rs
+++ src/app.rs
@@ -7,12 +7,16 @@ use ratatui::{
     },
     prelude::*,
     symbols::Marker,
+    text::ToLine,
     widgets::{
         Block, Clear, Padding, Paragraph,
-        canvas::{Canvas, Circle, Line as CLine, Rectangle},
+        canvas::{self, Canvas, Circle, Line as CLine, Rectangle},
     },
 };
-use std::{io, time::Duration};
+use std::{
+    io,
+    time::{Duration, Instant},
+};
 use tui_widgets::big_text::{BigText, PixelSize};
 
 use crate::gamepad::Gamepad;
@@ -123,7 +127,8 @@ impl Widget for &App {
 
         let block = Block::bordered()
             .title(title.centered())
-            .title_bottom(" quit: 'q', debug info: 'd' ".dim().into_centered_line());
+            .title_bottom(" quit: 'q', debug info: 'd' ".to_line().centered())
+            .padding(Padding::uniform(10));
 
         if self.show_debug_info {
             Paragraph::new(format!("{:#?}", self.gamepad))
@@ -132,8 +137,12 @@ impl Widget for &App {
             return;
         }
 
-        const GAMEPAD_AREA_ASPECT_RATIO: f32 = 4.8; // source: it was revealed to me in a dream
-        let mut gamepad_area = block.inner(area);
+        let [mut gamepad_area, details_area] = block.inner(area).layout(&Layout::vertical([
+            Constraint::Fill(1),
+            Constraint::Length(1),
+        ]));
+
+        const GAMEPAD_AREA_ASPECT_RATIO: f32 = 3.5; // source: it was revealed to me in a dream
 
         let target_height = (gamepad_area.width as f32 / GAMEPAD_AREA_ASPECT_RATIO) as u16;
 
@@ -147,285 +156,10 @@ impl Widget for &App {
         }
 
         Canvas::default()
-            .x_bounds([-45., 45.])
-            .y_bounds([-13., 25.])
+            .x_bounds([-30., 30.])
+            .y_bounds([-10., 23.])
             .marker(Marker::Octant)
-            .paint(|ctx| {
-                const CONTROLLER_TOP_Y: f64 = 12.;
-                const CONTROLLER_TOP_X: f64 = 23.;
-                const CONTROLLER_BOTTOM_Y: f64 = -5.;
-                const CONTROLLER_BOTTOM_X: f64 = 15.;
-                const HANDLE_BOTTOM_Y: f64 = -10.;
-                const HANDLE_BOTTOM_X: f64 = 30.;
-
-                // top
-                ctx.draw(&CLine::new(
-                    -CONTROLLER_TOP_X,
-                    CONTROLLER_TOP_Y,
-                    CONTROLLER_TOP_X,
-                    CONTROLLER_TOP_Y,
-                    Color::DarkGray,
-                ));
-                // bottom
-                ctx.draw(&CLine::new(
-                    -CONTROLLER_BOTTOM_X,
-                    CONTROLLER_BOTTOM_Y,
-                    CONTROLLER_BOTTOM_X,
-                    CONTROLLER_BOTTOM_Y,
-                    Color::DarkGray,
-                ));
-                // left handle
-                ctx.draw(&CLine::new(
-                    -CONTROLLER_TOP_X,
-                    CONTROLLER_TOP_Y,
-                    -HANDLE_BOTTOM_X,
-                    HANDLE_BOTTOM_Y,
-                    Color::DarkGray,
-                ));
-                ctx.draw(&CLine::new(
-                    -CONTROLLER_BOTTOM_X,
-                    CONTROLLER_BOTTOM_Y,
-                    -HANDLE_BOTTOM_X,
-                    HANDLE_BOTTOM_Y,
-                    Color::DarkGray,
-                ));
-                // right handle
-                ctx.draw(&CLine::new(
-                    CONTROLLER_TOP_X,
-                    CONTROLLER_TOP_Y,
-                    HANDLE_BOTTOM_X,
-                    HANDLE_BOTTOM_Y,
-                    Color::DarkGray,
-                ));
-                ctx.draw(&CLine::new(
-                    CONTROLLER_BOTTOM_X,
-                    CONTROLLER_BOTTOM_Y,
-                    HANDLE_BOTTOM_X,
-                    HANDLE_BOTTOM_Y,
-                    Color::DarkGray,
-                ));
-
-                // shoulder buttons
-                // L1
-                ctx.draw(&Rectangle::new(
-                    -CONTROLLER_TOP_X + 2.,
-                    14.,
-                    10.,
-                    2.,
-                    if self.gamepad.buttons.left_trigger {
-                        Color::Yellow
-                    } else {
-                        Color::DarkGray
-                    },
-                ));
-                ctx.draw(&Rectangle::new(
-                    -CONTROLLER_TOP_X + 5.,
-                    18.,
-                    3.,
-                    5.,
-                    if self.gamepad.buttons.left_trigger2 {
-                        Color::Yellow
-                    } else {
-                        Color::DarkGray
-                    },
-                ));
-
-                // R1
-                ctx.draw(&Rectangle::new(
-                    CONTROLLER_TOP_X - 2. - 10.,
-                    14.,
-                    10.,
-                    2.,
-                    if self.gamepad.buttons.right_trigger {
-                        Color::Yellow
-                    } else {
-                        Color::DarkGray
-                    },
-                ));
-                ctx.draw(&Rectangle::new(
-                    CONTROLLER_TOP_X - 5. - 3.,
-                    18.,
-                    3.,
-                    5.,
-                    if self.gamepad.buttons.right_trigger2 {
-                        Color::Yellow
-                    } else {
-                        Color::DarkGray
-                    },
-                ));
-
-                // d-pad
-                ctx.draw(&Circle::new(
-                    -17.5,
-                    8.5,
-                    1.1,
-                    if self.gamepad.buttons.d_pad_up {
-                        Color::Yellow
-                    } else {
-                        Color::DarkGray
-                    },
-                ));
-                ctx.draw(&Circle::new(
-                    -15.,
-                    6.,
-                    1.1,
-                    if self.gamepad.buttons.d_pad_right {
-                        Color::Yellow
-                    } else {
-                        Color::DarkGray
-                    },
-                ));
-                ctx.draw(&Circle::new(
-                    -20.,
-                    6.,
-                    1.1,
-                    if self.gamepad.buttons.d_pad_left {
-                        Color::Yellow
-                    } else {
-                        Color::DarkGray
-                    },
-                ));
-                ctx.draw(&Circle::new(
-                    -17.5,
-                    3.5,
-                    1.1,
-                    if self.gamepad.buttons.d_pad_down {
-                        Color::Yellow
-                    } else {
-                        Color::DarkGray
-                    },
-                ));
-
-                // action pad
-                ctx.draw(&Circle::new(
-                    17.5,
-                    8.5,
-                    1.1,
-                    if self.gamepad.buttons.north {
-                        Color::Yellow
-                    } else {
-                        Color::DarkGray
-                    },
-                ));
-                ctx.draw(&Circle::new(
-                    15.,
-                    6.,
-                    1.1,
-                    if self.gamepad.buttons.west {
-                        Color::Yellow
-                    } else {
-                        Color::DarkGray
-                    },
-                ));
-                ctx.draw(&Circle::new(
-                    20.,
-                    6.,
-                    1.1,
-                    if self.gamepad.buttons.east {
-                        Color::Yellow
-                    } else {
-                        Color::DarkGray
-                    },
-                ));
-                ctx.draw(&Circle::new(
-                    17.5,
-                    3.5,
-                    1.1,
-                    if self.gamepad.buttons.south {
-                        Color::Yellow
-                    } else {
-                        Color::DarkGray
-                    },
-                ));
-
-                // start/select
-                ctx.draw(&Rectangle::new(
-                    -7.,
-                    5.,
-                    3.,
-                    1.5,
-                    if self.gamepad.buttons.select {
-                        Color::Yellow
-                    } else {
-                        Color::DarkGray
-                    },
-                ));
-                ctx.draw(&Rectangle::new(
-                    4.,
-                    5.,
-                    3.,
-                    1.5,
-                    if self.gamepad.buttons.start {
-                        Color::Yellow
-                    } else {
-                        Color::DarkGray
-                    },
-                ));
-
-                // mode (center button, ps-button, home button etc)
-                ctx.draw(&Circle::new(
-                    0.,
-                    4.,
-                    1.1,
-                    if self.gamepad.buttons.mode {
-                        Color::Yellow
-                    } else {
-                        Color::DarkGray
-                    },
-                ));
-
-                const JOYSTICK_OUTER_RADIUS: f64 = 3.;
-                const JOYSTICK_MOVEMENT: f64 = 0.5 * JOYSTICK_OUTER_RADIUS;
-                const JOYSTICK_INNER_RADIUS: f64 = 0.5 * JOYSTICK_OUTER_RADIUS;
-                const JOYSTICK_X: f64 = 10.;
-                const JOYSTICK_Y: f64 = 0.;
-
-                let mut left_joystick = Circle::new(
-                    -JOYSTICK_X,
-                    JOYSTICK_Y,
-                    JOYSTICK_OUTER_RADIUS,
-                    Color::DarkGray,
-                );
-                ctx.draw(&left_joystick);
-                left_joystick.x += JOYSTICK_MOVEMENT * self.gamepad.axises.left_stick_x as f64;
-                left_joystick.y += JOYSTICK_MOVEMENT * self.gamepad.axises.left_stick_y as f64;
-                left_joystick.radius = JOYSTICK_INNER_RADIUS;
-                left_joystick.color = if self.gamepad.buttons.left_thumb {
-                    Color::Yellow
-                } else if self.gamepad.left_stick_active() {
-                    Color::Cyan
-                } else {
-                    Color::White
-                };
-                ctx.draw(&left_joystick);
-                if self.gamepad.buttons.left_thumb {
-                    left_joystick.radius = 1.;
-                    ctx.draw(&left_joystick);
-                }
-
-                let mut right_joystick = Circle::new(
-                    JOYSTICK_X,
-                    JOYSTICK_Y,
-                    JOYSTICK_OUTER_RADIUS,
-                    Color::DarkGray,
-                );
-                ctx.draw(&right_joystick);
-                right_joystick.x += JOYSTICK_MOVEMENT * self.gamepad.axises.right_stick_x as f64;
-                right_joystick.y += JOYSTICK_MOVEMENT * self.gamepad.axises.right_stick_y as f64;
-                right_joystick.radius = JOYSTICK_INNER_RADIUS;
-                right_joystick.color = if self.gamepad.buttons.right_thumb {
-                    Color::Yellow
-                } else if self.gamepad.right_stick_active() {
-                    Color::Cyan
-                } else {
-                    Color::White
-                };
-                ctx.draw(&right_joystick);
-                if self.gamepad.buttons.right_thumb {
-                    right_joystick.radius = 1.;
-                    ctx.draw(&right_joystick);
-                }
-            })
+            .paint(|ctx| self.gamepad.paint_to_canvas(ctx))
             .render(gamepad_area, buf);
 
         if self.gamepad.check_if_konami_code_is_entered() {
--- src/gamepad.rs
+++ src/gamepad.rs
@@ -1,4 +1,18 @@
 use gilrs::{Button, GamepadId};
+use ratatui::{
+    DefaultTerminal,
+    crossterm::{
+        self,
+        event::{KeyCode, KeyEvent, KeyEventKind, KeyModifiers},
+    },
+    prelude::*,
+    symbols::Marker,
+    text::ToLine,
+    widgets::{
+        Block, Clear, Padding, Paragraph,
+        canvas::{self, Canvas, Circle, Line as CLine, Rectangle},
+    },
+};
 
 const KONAMI_CODE: [Button; 10] = [
     Button::DPadUp,
@@ -31,6 +45,283 @@ impl Gamepad {
 
     pub fn right_stick_active(&self) -> bool {
         self.axises.right_stick_x.hypot(self.axises.right_stick_y) > Self::STICK_ACTIVATION_HYPOT
+    }
+
+    pub fn paint_to_canvas(&self, ctx: &mut canvas::Context) {
+        const CONTROLLER_TOP_Y: f64 = 12.;
+        const CONTROLLER_TOP_X: f64 = 23.;
+        const CONTROLLER_BOTTOM_Y: f64 = -5.;
+        const CONTROLLER_BOTTOM_X: f64 = 15.;
+        const HANDLE_BOTTOM_Y: f64 = -10.;
+        const HANDLE_BOTTOM_X: f64 = 30.;
+
+        // top
+        ctx.draw(&CLine::new(
+            -CONTROLLER_TOP_X,
+            CONTROLLER_TOP_Y,
+            CONTROLLER_TOP_X,
+            CONTROLLER_TOP_Y,
+            Color::DarkGray,
+        ));
+        // bottom
+        ctx.draw(&CLine::new(
+            -CONTROLLER_BOTTOM_X,
+            CONTROLLER_BOTTOM_Y,
+            CONTROLLER_BOTTOM_X,
+            CONTROLLER_BOTTOM_Y,
+            Color::DarkGray,
+        ));
+        // left handle
+        ctx.draw(&CLine::new(
+            -CONTROLLER_TOP_X,
+            CONTROLLER_TOP_Y,
+            -HANDLE_BOTTOM_X,
+            HANDLE_BOTTOM_Y,
+            Color::DarkGray,
+        ));
+        ctx.draw(&CLine::new(
+            -CONTROLLER_BOTTOM_X,
+            CONTROLLER_BOTTOM_Y,
+            -HANDLE_BOTTOM_X,
+            HANDLE_BOTTOM_Y,
+            Color::DarkGray,
+        ));
+        // right handle
+        ctx.draw(&CLine::new(
+            CONTROLLER_TOP_X,
+            CONTROLLER_TOP_Y,
+            HANDLE_BOTTOM_X,
+            HANDLE_BOTTOM_Y,
+            Color::DarkGray,
+        ));
+        ctx.draw(&CLine::new(
+            CONTROLLER_BOTTOM_X,
+            CONTROLLER_BOTTOM_Y,
+            HANDLE_BOTTOM_X,
+            HANDLE_BOTTOM_Y,
+            Color::DarkGray,
+        ));
+
+        // shoulder buttons
+        // L1
+        ctx.draw(&Rectangle::new(
+            -CONTROLLER_TOP_X + 2.,
+            14.,
+            10.,
+            2.,
+            if self.buttons.left_trigger {
+                Color::Yellow
+            } else {
+                Color::DarkGray
+            },
+        ));
+        ctx.draw(&Rectangle::new(
+            -CONTROLLER_TOP_X + 5.,
+            18.,
+            3.,
+            5.,
+            if self.buttons.left_trigger2 {
+                Color::Yellow
+            } else {
+                Color::DarkGray
+            },
+        ));
+
+        // R1
+        ctx.draw(&Rectangle::new(
+            CONTROLLER_TOP_X - 2. - 10.,
+            14.,
+            10.,
+            2.,
+            if self.buttons.right_trigger {
+                Color::Yellow
+            } else {
+                Color::DarkGray
+            },
+        ));
+        ctx.draw(&Rectangle::new(
+            CONTROLLER_TOP_X - 5. - 3.,
+            18.,
+            3.,
+            5.,
+            if self.buttons.right_trigger2 {
+                Color::Yellow
+            } else {
+                Color::DarkGray
+            },
+        ));
+
+        // d-pad
+        ctx.draw(&Circle::new(
+            -17.5,
+            8.5,
+            1.1,
+            if self.buttons.d_pad_up {
+                Color::Yellow
+            } else {
+                Color::DarkGray
+            },
+        ));
+        ctx.draw(&Circle::new(
+            -15.,
+            6.,
+            1.1,
+            if self.buttons.d_pad_right {
+                Color::Yellow
+            } else {
+                Color::DarkGray
+            },
+        ));
+        ctx.draw(&Circle::new(
+            -20.,
+            6.,
+            1.1,
+            if self.buttons.d_pad_left {
+                Color::Yellow
+            } else {
+                Color::DarkGray
+            },
+        ));
+        ctx.draw(&Circle::new(
+            -17.5,
+            3.5,
+            1.1,
+            if self.buttons.d_pad_down {
+                Color::Yellow
+            } else {
+                Color::DarkGray
+            },
+        ));
+
+        // action pad
+        ctx.draw(&Circle::new(
+            17.5,
+            8.5,
+            1.1,
+            if self.buttons.north {
+                Color::Yellow
+            } else {
+                Color::DarkGray
+            },
+        ));
+        ctx.draw(&Circle::new(
+            15.,
+            6.,
+            1.1,
+            if self.buttons.west {
+                Color::Yellow
+            } else {
+                Color::DarkGray
+            },
+        ));
+        ctx.draw(&Circle::new(
+            20.,
+            6.,
+            1.1,
+            if self.buttons.east {
+                Color::Yellow
+            } else {
+                Color::DarkGray
+            },
+        ));
+        ctx.draw(&Circle::new(
+            17.5,
+            3.5,
+            1.1,
+            if self.buttons.south {
+                Color::Yellow
+            } else {
+                Color::DarkGray
+            },
+        ));
+
+        // start/select
+        ctx.draw(&Rectangle::new(
+            -7.,
+            5.,
+            3.,
+            1.5,
+            if self.buttons.select {
+                Color::Yellow
+            } else {
+                Color::DarkGray
+            },
+        ));
+        ctx.draw(&Rectangle::new(
+            4.,
+            5.,
+            3.,
+            1.5,
+            if self.buttons.start {
+                Color::Yellow
+            } else {
+                Color::DarkGray
+            },
+        ));
+
+        // mode (center button, ps-button, home button etc)
+        ctx.draw(&Circle::new(
+            0.,
+            4.,
+            1.1,
+            if self.buttons.mode {
+                Color::Yellow
+            } else {
+                Color::DarkGray
+            },
+        ));
+
+        const JOYSTICK_OUTER_RADIUS: f64 = 3.;
+        const JOYSTICK_MOVEMENT: f64 = 0.5 * JOYSTICK_OUTER_RADIUS;
+        const JOYSTICK_INNER_RADIUS: f64 = 0.5 * JOYSTICK_OUTER_RADIUS;
+        const JOYSTICK_X: f64 = 10.;
+        const JOYSTICK_Y: f64 = 0.;
+
+        let mut left_joystick = Circle::new(
+            -JOYSTICK_X,
+            JOYSTICK_Y,
+            JOYSTICK_OUTER_RADIUS,
+            Color::DarkGray,
+        );
+        ctx.draw(&left_joystick);
+        left_joystick.x += JOYSTICK_MOVEMENT * self.axises.left_stick_x as f64;
+        left_joystick.y += JOYSTICK_MOVEMENT * self.axises.left_stick_y as f64;
+        left_joystick.radius = JOYSTICK_INNER_RADIUS;
+        left_joystick.color = if self.buttons.left_thumb {
+            Color::Yellow
+        } else if self.left_stick_active() {
+            Color::Cyan
+        } else {
+            Color::White
+        };
+        ctx.draw(&left_joystick);
+        if self.buttons.left_thumb {
+            left_joystick.radius = 1.;
+            ctx.draw(&left_joystick);
+        }
+
+        let mut right_joystick = Circle::new(
+            JOYSTICK_X,
+            JOYSTICK_Y,
+            JOYSTICK_OUTER_RADIUS,
+            Color::DarkGray,
+        );
+        ctx.draw(&right_joystick);
+        right_joystick.x += JOYSTICK_MOVEMENT * self.axises.right_stick_x as f64;
+        right_joystick.y += JOYSTICK_MOVEMENT * self.axises.right_stick_y as f64;
+        right_joystick.radius = JOYSTICK_INNER_RADIUS;
+        right_joystick.color = if self.buttons.right_thumb {
+            Color::Yellow
+        } else if self.right_stick_active() {
+            Color::Cyan
+        } else {
+            Color::White
+        };
+        ctx.draw(&right_joystick);
+        if self.buttons.right_thumb {
+            right_joystick.radius = 1.;
+            ctx.draw(&right_joystick);
+        }
     }
 }
 
